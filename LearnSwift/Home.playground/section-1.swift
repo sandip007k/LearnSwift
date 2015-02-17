d3cc9953424d5d2ad3136177b723c76714b51a78
sandip007k <sandip@vismaad.com>
2015-02-17 12:47:25 +0530

    Learn Swift Code
    Ever

---
+++ LearnSwift/Home.playground/section-1.swift
@@ -0,0 +1,31 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let name="sandip"
+var orange=5
+var banan=10
+
+println("Number of Fruit i have \(orange+banan)");
+
+
+
+
+
+var shooping=["hye","kida","working"]
+
+shooping[1]="sandip"
+
+println(shooping)
+
+
+
+let indiviualscrore = [75, 89, 99]
+
+var ​teamScore​=0
+
+
+
+
+
---
+++ LearnSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LearnSwift
+//
+//  Created by SANDIP KAUSHAL on 23/01/15.
+//  Copyright (c) 2015 Vismaad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("Sandip Kaushal")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
